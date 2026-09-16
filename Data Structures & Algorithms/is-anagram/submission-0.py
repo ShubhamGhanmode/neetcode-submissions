3bebf49196e79d64bd05c1d5dff1b2373f7cd65b
Shubham Ghanmode <sghanmode4@gmail.com>
2026-09-16 20:56:15 +0100

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sdict = {}
+        tdict = {}
+
+        for i in s:
+            if i in sdict:
+                sdict[i] += 1
+            else:
+                sdict[i] = 1
+        for j in t:
+            if j in tdict:
+                tdict[j] += 1
+            else:
+                tdict[j] = 1
+        if sdict == tdict:
+            return True
+        else:
+            return False
